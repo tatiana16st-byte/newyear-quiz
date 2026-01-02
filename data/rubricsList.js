00386336bc690f6eb6e37158dccc1e89c5a0f9b8
tatiana16st-byte <tatiana16st@gmail.com>
2026-01-02 19:59:36 +0300

    Update rubricsList.js

--- data/rubricsList.js
+++ data/rubricsList.js
@@ -1,22 +1,36 @@
+/**
+ * Главный файл конфигурации рубрик викторины.
+ * Ссылается на отдельные файлы с вопросами.
+ */
+
+const movies = require('./movies');
+const fatherfrost = require('./fatherfrost');
+const traditions = require('./traditions');
+const tree = require('./tree');
+
 module.exports = [
   {
     id: 1,
     title: 'Новогодние мультфильмы и фильмы',
-    file: './movies'
+    rubricId: 'movies',
+    data: movies
   },
   {
     id: 2,
     title: 'Дед Мороз',
-    file: './fatherfrost'
+    rubricId: 'fatherfrost',
+    data: fatherfrost
   },
   {
     id: 3,
     title: 'Новогодние традиции',
-    file: './traditions'
+    rubricId: 'traditions',
+    data: traditions
   },
   {
     id: 4,
     title: 'Новогодняя ёлка',
-    file: './tree'
+    rubricId: 'tree',
+    data: tree
   }
 ];
