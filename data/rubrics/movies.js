daee5aa001f386d1f7f472d3ce0c5c0d5ae281ba
tatiana16st-byte <tatiana16st@gmail.com>
2026-01-02 17:28:50 +0300

    Create movies.js

---
+++ data/rubrics/movies.js
@@ -0,0 +1,396 @@
+module.exports = {
+  "rubricId": 1,
+  "rubricTitle": "Новогодние мультфильмы и фильмы",
+  "questions": [
+    {
+      "id": 1,
+      "question": "Почему Кевин остался один дома?",
+      "options": {
+        "A": "Он убежал",
+        "B": "Его забыли родители",
+        "C": "Он спрятался",
+        "D": "Он опоздал в аэропорт"
+      },
+      "correctAnswer": "B",
+      "correctText": "Его забыли родители",
+      "imagePath": "/images/movies/question01.png"
+    },
+    {
+      "id": 2,
+      "question": "Почему Герда отправляется в путешествие в «Снежной королеве»?",
+      "options": {
+        "A": "Найти волшебство",
+        "B": "Вернуться домой",
+        "C": "Спасти Кая",
+        "D": "Победить королеву"
+      },
+      "correctAnswer": "C",
+      "correctText": "Спасти Кая",
+      "imagePath": "/images/movies/question02.png"
+    },
+    {
+      "id": 3,
+      "question": "Что попросила падчерица у двенадцати месяцев?",
+      "options": {
+        "A": "Золото",
+        "B": "Тёплую одежду",
+        "C": "Цветы",
+        "D": "Еду"
+      },
+      "correctAnswer": "C",
+      "correctText": "Цветы",
+      "imagePath": "/images/movies/question03.png"
+    },
+    {
+      "id": 4,
+      "question": "Кто пытался ограбить дом Кевина?",
+      "options": {
+        "A": "Полицейские",
+        "B": "Соседи",
+        "C": "Два грабителя",
+        "D": "Почтальоны"
+      },
+      "correctAnswer": "C",
+      "correctText": "Два грабителя",
+      "imagePath": "/images/movies/question04.png"
+    },
+    {
+      "id": 5,
+      "question": "С кем сражался Щелкунчик в новогоднюю ночь?",
+      "options": {
+        "A": "С драконом",
+        "B": "С Мышиным королём",
+        "C": "С волшебником",
+        "D": "Со Снеговиком"
+      },
+      "correctAnswer": "B",
+      "correctText": "С Мышиным королём",
+      "imagePath": "/images/movies/question05.png"
+    },
+    {
+      "id": 6,
+      "question": "Почему Гринч ненавидел Рождество?",
+      "options": {
+        "A": "Он не верил в чудо",
+        "B": "Он не любил подарки",
+        "C": "Его обидели в детстве",
+        "D": "Он был злым волшебником"
+      },
+      "correctAnswer": "C",
+      "correctText": "Его обидели в детстве",
+      "imagePath": "/images/movies/question06.png"
+    },
+    {
+      "id": 7,
+      "question": "Зачем Снеговик отправился в путь в мультфильме «Снеговик-почтовик»?",
+      "options": {
+        "A": "Искать друзей",
+        "B": "Найти Новый год",
+        "C": "Доставить письмо",
+        "D": "Принести подарки"
+      },
+      "correctAnswer": "C",
+      "correctText": "Доставить письмо",
+      "imagePath": "/images/movies/question07.png"
+    },
+    {
+      "id": 8,
+      "question": "Куда направляется поезд в «Полярном экспрессе»?",
+      "options": {
+        "A": "В Рождественский город",
+        "B": "К Деду Морозу",
+        "C": "На Северный полюс",
+        "D": "В сказочный лес"
+      },
+      "correctAnswer": "C",
+      "correctText": "На Северный полюс",
+      "imagePath": "/images/movies/question08.png"
+    },
+    {
+      "id": 9,
+      "question": "Кто является главным героем мультфильма «Клаус»?",
+      "options": {
+        "A": "Санта-Клаус",
+        "B": "Эльф",
+        "C": "Почтальон",
+        "D": "Олень"
+      },
+      "correctAnswer": "C",
+      "correctText": "Почтальон",
+      "imagePath": "/images/movies/question09.png"
+    },
+    {
+      "id": 10,
+      "question": "Что произошло с сердцем Кая после чар Снежной королевы?",
+      "options": {
+        "A": "Оно исчезло",
+        "B": "Оно стало каменным",
+        "C": "Оно замёрзло",
+        "D": "Оно перестало биться"
+      },
+      "correctAnswer": "C",
+      "correctText": "Оно замёрзло",
+      "imagePath": "/images/movies/question10.png"
+    },
+    {
+      "id": 11,
+      "question": "За какие качества Морозко наградил Настеньку?",
+      "options": {
+        "A": "Смелость",
+        "B": "Хитрость",
+        "C": "Доброту и трудолюбие",
+        "D": "Красоту"
+      },
+      "correctAnswer": "C",
+      "correctText": "Доброту и трудолюбие",
+      "imagePath": "/images/movies/question11.png"
+    },
+    {
+      "id": 12,
+      "question": "Кто помог Деду Морозу в мультфильме «Дед Мороз и серый волк»?",
+      "options": {
+        "A": "Снегурочка",
+        "B": "Дети",
+        "C": "Лесные звери",
+        "D": "Снеговик"
+      },
+      "correctAnswer": "C",
+      "correctText": "Лесные звери",
+      "imagePath": "/images/movies/question12.png"
+    },
+    {
+      "id": 13,
+      "question": "Почему Умка отправился искать друга?",
+      "options": {
+        "A": "Он потерялся",
+        "B": "Ему стало одиноко",
+        "C": "Его позвала мама",
+        "D": "Он хотел приключений"
+      },
+      "correctAnswer": "B",
+      "correctText": "Ему стало одиноко",
+      "imagePath": "/images/movies/question13.png"
+    },
+    {
+      "id": 14,
+      "question": "Кто пытался испортить праздник в мультфильме «Дед Мороз и серый волк»?",
+      "options": {
+        "A": "Лиса",
+        "B": "Медведь",
+        "C": "Волк",
+        "D": "Кот"
+      },
+      "correctAnswer": "C",
+      "correctText": "Волк",
+      "imagePath": "/images/movies/question14.png"
+    },
+    {
+      "id": 15,
+      "question": "Что решил сделать Джек Скеллингтон после знакомства с Рождеством?",
+      "options": {
+        "A": "Украсть праздник",
+        "B": "Отменить Хэллоуин",
+        "C": "Стать Санта-Клаусом",
+        "D": "Запретить Рождество"
+      },
+      "correctAnswer": "C",
+      "correctText": "Стать Санта-Клаусом",
+      "imagePath": "/images/movies/question15.png"
+    },
+    {
+      "id": 16,
+      "question": "Почему падчерицу отправили в лес зимой?",
+      "options": {
+        "A": "За лень",
+        "B": "За обман",
+        "C": "По приказу мачехи",
+        "D": "За плохие оценки"
+      },
+      "correctAnswer": "C",
+      "correctText": "По приказу мачехи",
+      "imagePath": "/images/movies/question16.png"
+    },
+    {
+      "id": 17,
+      "question": "Что символизирует «Полярный экспресс»?",
+      "options": {
+        "A": "Скорость",
+        "B": "Дружбу",
+        "C": "Веру в чудо",
+        "D": "Путешествие"
+      },
+      "correctAnswer": "C",
+      "correctText": "Веру в чудо",
+      "imagePath": "/images/movies/question17.png"
+    },
+    {
+      "id": 18,
+      "question": "Что лежит в основе сюжета «Холодного сердца»?",
+      "options": {
+        "A": "Борьба за трон",
+        "B": "Магия льда",
+        "C": "Любовь между сёстрами",
+        "D": "Победа над врагом"
+      },
+      "correctAnswer": "C",
+      "correctText": "Любовь между сёстрами",
+      "imagePath": "/images/movies/question18.png"
+    },
+    {
+      "id": 19,
+      "question": "Какую посылку нёс Снеговик-почтовик?",
+      "options": {
+        "A": "Подарки детям",
+        "B": "Письмо Деду Морозу",
+        "C": "Волшебный мешок",
+        "D": "Список желаний"
+      },
+      "correctAnswer": "B",
+      "correctText": "Письмо Деду Морозу",
+      "imagePath": "/images/movies/question19.png"
+    },
+    {
+      "id": 20,
+      "question": "Кто является главным героем «Рождественской истории»?",
+      "options": {
+        "A": "Волшебник",
+        "B": "Санта",
+        "C": "Скрудж",
+        "D": "Эльф"
+      },
+      "correctAnswer": "C",
+      "correctText": "Скрудж",
+      "imagePath": "/images/movies/question20.png"
+    },
+    {
+      "id": 21,
+      "question": "Каким был Скрудж в начале истории?",
+      "options": {
+        "A": "Добрым",
+        "B": "Весёлым",
+        "C": "Жадным",
+        "D": "Бедным"
+      },
+      "correctAnswer": "C",
+      "correctText": "Жадным",
+      "imagePath": "/images/movies/question21.png"
+    },
+    {
+      "id": 22,
+      "question": "Почему Матроскин и Шарик поссорились в «Зиме в Простоквашино»?",
+      "options": {
+        "A": "Из-за подарков",
+        "B": "Из-за коровы",
+        "C": "Из-за письма",
+        "D": "Из-за дома"
+      },
+      "correctAnswer": "B",
+      "correctText": "Из-за коровы",
+      "imagePath": "/images/movies/question22.png"
+    },
+    {
+      "id": 23,
+      "question": "Что помогло Гринчу измениться?",
+      "options": {
+        "A": "Подарки",
+        "B": "Волшебство",
+        "C": "Доброта людей",
+        "D": "Одиночество"
+      },
+      "correctAnswer": "C",
+      "correctText": "Доброта людей",
+      "imagePath": "/images/movies/question23.png"
+    },
+    {
+      "id": 24,
+      "question": "Кто становится другом Умки?",
+      "options": {
+        "A": "Заяц",
+        "B": "Медвежонок",
+        "C": "Человеческий мальчик",
+        "D": "Пингвин"
+      },
+      "correctAnswer": "C",
+      "correctText": "Человеческий мальчик",
+      "imagePath": "/images/movies/question24.png"
+    },
+    {
+      "id": 25,
+      "question": "Что стало наградой падчерице после встречи с месяцами?",
+      "options": {
+        "A": "Деньги",
+        "B": "Тёплый дом",
+        "C": "Цветы и подарки",
+        "D": "Новая семья"
+      },
+      "correctAnswer": "C",
+      "correctText": "Цветы и подарки",
+      "imagePath": "/images/movies/question25.png"
+    },
+    {
+      "id": 26,
+      "question": "Кто заколдовал Щелкунчика?",
+      "options": {
+        "A": "Дроссельмейер",
+        "B": "Снежная королева",
+        "C": "Мышиная королева",
+        "D": "Ведьма"
+      },
+      "correctAnswer": "C",
+      "correctText": "Мышиная королева",
+      "imagePath": "/images/movies/question26.png"
+    },
+    {
+      "id": 27,
+      "question": "Почему Джек Скеллингтон устал от Хэллоуина?",
+      "options": {
+        "A": "Его никто не боялся",
+        "B": "Он хотел перемен",
+        "C": "Он потерял друзей",
+        "D": "Он стал злым"
+      },
+      "correctAnswer": "B",
+      "correctText": "Он хотел перемен",
+      "imagePath": "/images/movies/question27.png"
+    },
+    {
+      "id": 28,
+      "question": "Какую миссию выполняет Артур в мультфильме «Секретная служба Санта-Клауса»?",
+      "options": {
+        "A": "Спасти Рождество",
+        "B": "Найти подарок для девочки",
+        "C": "Починить сани",
+        "D": "Найти Санту"
+      },
+      "correctAnswer": "B",
+      "correctText": "Найти подарок для девочки",
+      "imagePath": "/images/movies/question28.png"
+    },
+    {
+      "id": 29,
+      "question": "Кто первым поверил в чудо в «Полярном экспрессе»?",
+      "options": {
+        "A": "Машинист",
+        "B": "Девочка",
+        "C": "Главный мальчик",
+        "D": "Санта"
+      },
+      "correctAnswer": "C",
+      "correctText": "Главный мальчик",
+      "imagePath": "/images/movies/question29.png"
+    },
+    {
+      "id": 30,
+      "question": "Чем заканчивается «Снежная королева»?",
+      "options": {
+        "A": "Победой королевы",
+        "B": "Исчезновением Кая",
+        "C": "Возвращением Кая домой",
+        "D": "Новым путешествием"
+      },
+      "correctAnswer": "C",
+      "correctText": "Возвращением Кая домой",
+      "imagePath": "/images/movies/question30.png"
+}
+  ]
+};
