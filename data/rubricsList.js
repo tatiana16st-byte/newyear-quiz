e8c87a4c115f09ac0fa55446a63e060aeb94f5a1
tatiana16st-byte <tatiana16st@gmail.com>
2026-01-02 17:38:17 +0300

    Create rubricsList.js

---
+++ data/rubricsList.js
@@ -0,0 +1,22 @@
+module.exports = [
+  {
+    id: "movies",
+    title: "🎬 Новогодние мультфильмы и фильмы",
+    file: "./rubrics/movies"
+  },
+  {
+    id: "fatherfrost",
+    title: "🎅 Дед Мороз",
+    file: "./rubrics/fatherfrost"
+  },
+  {
+    id: "traditions",
+    title: "🎄 Новогодние традиции",
+    file: "./rubrics/traditions"
+  },
+  {
+    id: "tree",
+    title: "🌲 Ёлка и украшения",
+    file: "./rubrics/tree"
+  }
+];
