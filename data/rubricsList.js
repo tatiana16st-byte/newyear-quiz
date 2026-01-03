bfc38136dcc551b8955559936f6a1af079226edb
tatiana16st-byte <tatiana16st@gmail.com>
2026-01-03 22:38:58 +0300

    Update rubricsList.js

--- data/rubricsList.js
+++ data/rubricsList.js
@@ -2,21 +2,25 @@ module.exports = [
   {
     id: 1,
     title: "Новогодние фильмы и мультфильмы",
-    image: "/images/movies.png"
+    image: "/images/movies.png",
+    file: "movies.js"
   },
   {
     id: 2,
     title: "Дед Мороз",
-    image: "/images/fatherfrost.png"
+    image: "/images/fatherfrost.js",
+    file: "fatherfrost.js"
   },
   {
     id: 3,
     title: "Новогодние традиции",
-    image: "/images/traditions.png"
+    image: "/images/traditions.png",
+    file: "traditions.js"
   },
   {
     id: 4,
     title: "Ёлка",
-    image: "/images/tree.png"
+    image: "/images/tree.png",
+    file: "tree.js"
   }
 ];
