da524111b3f06e50462b7bac0dbf111470874417
tatiana16st-byte <tatiana16st@gmail.com>
2026-01-03 09:36:19 +0300

    Update rubricsList.js

--- data/rubricsList.js
+++ data/rubricsList.js
@@ -1,8 +1,63 @@
-// rubricsList.js
-const movies = require('./rubrics/movies');
-const fatherfrost = require('./rubrics/fatherfrost');
-const traditions = require('./rubrics/traditions');
-const tree = require('./rubrics/tree');
+/**
+ * Главный файл конфигурации рубрик (rubricsList.js)
+ * Путь к файлу в проекте: /src/data/rubricsList.js
+ * Файлы вопросов находятся в: /data/rubrics/ (в корне проекта)
+ */
 
-const rubricsList = [movies, fatherfrost, traditions, tree];
-if (typeof module !== 'undefined') module.exports = rubricsList;
+const path = require('path');
+
+let moviesData, fatherfrostData, traditionsData, treeData;
+
+try {
+  // Загружаем данные из файлов, используя правильный путь относительно /src/data/
+  moviesData = require(path.join(__dirname, '../../data/rubrics/movies'));
+  fatherfrostData = require(path.join(__dirname, '../../data/rubrics/fatherfrost'));
+  traditionsData = require(path.join(__dirname, '../../data/rubrics/traditions'));
+  treeData = require(path.join(__dirname, '../../data/rubrics/tree'));
+} catch (e) {
+  console.error("!!! КРИТИЧЕСКАЯ ОШИБКА: Файлы рубрик не найдены по пути /data/rubrics/");
+  console.error("Проверьте наличие файлов в GitHub по пути data/rubrics/");
+  throw e;
+}
+
+/**
+ * Функция для извлечения массива вопросов.
+ * Ваши файлы экспортируют объект { questions: [...] }, 
+ * поэтому мы достаем именно поле questions.
+ */
+const extractQuestions = (moduleExport) => {
+  if (moduleExport && moduleExport.questions) return moduleExport.questions;
+  if (Array.isArray(moduleExport)) return moduleExport;
+  return [];
+};
+
+const rubricsList = [
+  {
+    id: 1,
+    title: 'Новогодние мультфильмы и фильмы',
+    rubricId: 'movies',
+    questions: extractQuestions(moviesData)
+  },
+  {
+    id: 2,
+    title: 'Дед Мороз',
+    rubricId: 'fatherfrost',
+    questions: extractQuestions(fatherfrostData)
+  },
+  {
+    id: 3,
+    title: 'Новогодние традиции',
+    rubricId: 'traditions',
+    questions: extractQuestions(traditionsData)
+  },
+  {
+    id: 4,
+    title: 'Новогодняя ёлка',
+    rubricId: 'tree',
+    questions: extractQuestions(treeData)
+  }
+];
+
+if (typeof module !== 'undefined') {
+  module.exports = rubricsList;
+}
