ab504261610bffea1aed582e4c1936d290f1bb69
tatiana16st-byte <tatiana16st@gmail.com>
2026-01-03 09:27:14 +0300

    Update rubricsList.js

--- data/rubricsList.js
+++ data/rubricsList.js
@@ -1,45 +1,8 @@
-const path = require('path');
+// rubricsList.js
+const movies = require('./rubrics/movies');
+const fatherfrost = require('./rubrics/fatherfrost');
+const traditions = require('./rubrics/traditions');
+const tree = require('./rubrics/tree');
 
-let movies, fatherfrost, traditions, tree;
-
-try {
-  const baseDir = path.join(__dirname, '../../data/rubrics');
-
-  movies = require(path.join(baseDir, 'movies'));
-  fatherfrost = require(path.join(baseDir, 'fatherfrost'));
-  traditions = require(path.join(baseDir, 'traditions'));
-  tree = require(path.join(baseDir, 'tree'));
-
-} catch (e) {
-  console.error("!!! КРИТИЧЕСКАЯ ОШИБКА: Файлы рубрик не найдены по пути /data/rubrics/");
-  console.error("Попробуйте проверить, не заглавными ли буквами названы файлы в GitHub.");
-  console.error("Ошибка:", e.message);
-  throw e;
-}
-
-module.exports = [
-  {
-    id: 1,
-    title: 'Новогодние мультфильмы и фильмы',
-    rubricId: 'movies',
-    data: movies
-  },
-  {
-    id: 2,
-    title: 'Дед Мороз',
-    rubricId: 'fatherfrost',
-    data: fatherfrost
-  },
-  {
-    id: 3,
-    title: 'Новогодние традиции',
-    rubricId: 'traditions',
-    data: traditions
-  },
-  {
-    id: 4,
-    title: 'Новогодняя ёлка',
-    rubricId: 'tree',
-    data: tree
-  }
-];
+const rubricsList = [movies, fatherfrost, traditions, tree];
+if (typeof module !== 'undefined') module.exports = rubricsList;
